bf4a0a2e427747d854ac687ffb4a993ff424ba4a
Ellen Bennett <bennette831@gmail.com>
2023-09-20 16:36:50 -0600

    write functions that use different file I/O methods to mutate files

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='UTF-8') as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    text_file = open(f'{file_name}.txt', mode='a', encoding='UTF-8')
+    text_file.write(append_content)
+    text_file.close()
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding='UTF-8') as text_file:
+        for line in text_file:
+            return line
+
+   
